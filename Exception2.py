c3d7cb313e3ab91c2814efc57fed7ddc3387e3aa
Harsh Doshi <119748985+Harsh-D-2004@users.noreply.github.com>
2024-06-08 09:40:11 +0530

    Upp

--- Exception2.py
+++ Exception2.py
@@ -1,11 +1,11 @@
 def main():
-    
     No1 = int(input("Enter the first number : "))
     No2 = int(input("Enter the second number : "))
 
     Ans = No1 / No2
 
-    print("Division is : " , Ans)
+    print("Division is : ", Ans)
+
 
 if __name__ == "__main__":
     main()
